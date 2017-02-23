ab8866c0889c14baa9bb217d1fb3a1ddb4c57c41
HauyuChen <893769393@qq.com>
2017-02-24 07:45:46 +0800

    服务器完整源码
    服务器完整源码

---
+++ ParkingServer/parking.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace ParkingServer
+{
+    partial class SocketServer
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStartBind = new System.Windows.Forms.Button();
+            this.txtLog = new System.Windows.Forms.TextBox();
+            this.txtMsg = new System.Windows.Forms.TextBox();
+            this.btnSendMsg = new System.Windows.Forms.Button();
+            this.cboUserList = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnStartBind
+            // 
+            this.btnStartBind.Location = new System.Drawing.Point(237, 28);
+            this.btnStartBind.Name = "btnStartBind";
+            this.btnStartBind.Size = new System.Drawing.Size(111, 46);
+            this.btnStartBind.TabIndex = 0;
+            this.btnStartBind.Text = "启动服务器";
+            this.btnStartBind.UseVisualStyleBackColor = true;
+            this.btnStartBind.Click += new System.EventHandler(this.btnStartBind_Click);
+            // 
+            // txtLog
+            // 
+            this.txtLog.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.txtLog.Location = new System.Drawing.Point(39, 107);
+            this.txtLog.Multiline = true;
+            this.txtLog.Name = "txtLog";
+            this.txtLog.ReadOnly = true;
+            this.txtLog.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtLog.Size = new System.Drawing.Size(536, 303);
+            this.txtLog.TabIndex = 1;
+            // 
+            // txtMsg
+            // 
+            this.txtMsg.Location = new System.Drawing.Point(39, 461);
+            this.txtMsg.Multiline = true;
+            this.txtMsg.Name = "txtMsg";
+            this.txtMsg.Size = new System.Drawing.Size(455, 25);
+            this.txtMsg.TabIndex = 1;
+            // 
+            // btnSendMsg
+            // 
+            this.btnSendMsg.Location = new System.Drawing.Point(505, 435);
+            this.btnSendMsg.Name = "btnSendMsg";
+            this.btnSendMsg.Size = new System.Drawing.Size(70, 51);
+            this.btnSendMsg.TabIndex = 0;
+            this.btnSendMsg.Text = "发送消息";
+            this.btnSendMsg.UseVisualStyleBackColor = true;
+            this.btnSendMsg.Click += new System.EventHandler(this.btnSendMsg_Click);
+            // 
+            // cboUserList
+            // 
+            this.cboUserList.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboUserList.FormattingEnabled = true;
+            this.cboUserList.Location = new System.Drawing.Point(115, 435);
+            this.cboUserList.Name = "cboUserList";
+            this.cboUserList.Size = new System.Drawing.Size(138, 20);
+            this.cboUserList.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(37, 439);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 12);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "已连接端点：";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(39, 89);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 12);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "日志：";
+            // 
+            // SocketServer
+            // 
+            this.AcceptButton = this.btnSendMsg;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(604, 498);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cboUserList);
+            this.Controls.Add(this.txtMsg);
+            this.Controls.Add(this.txtLog);
+            this.Controls.Add(this.btnSendMsg);
+            this.Controls.Add(this.btnStartBind);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.Name = "SocketServer";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Parking";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.SocketServer_FormClosing);
+            this.Load += new System.EventHandler(this.SocketServer_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStartBind;
+        private System.Windows.Forms.TextBox txtLog;
+        private System.Windows.Forms.TextBox txtMsg;
+        private System.Windows.Forms.Button btnSendMsg;
+        private System.Windows.Forms.ComboBox cboUserList;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ ParkingServer/parking.cs
@@ -0,0 +1,333 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace ParkingServer
+{
+    public partial class SocketServer : Form
+    {
+        Socket socketWatch;
+        Socket socketSend;
+        Dictionary<string, Socket> dicSocket = new Dictionary<string, Socket>();
+        string ZigBee="";  //标识：ZigBee网络地址
+        string WeChat="";  //标识：WeChat网络地址
+        string WinPC="";   //标识：WinPC网络地址
+
+        public SocketServer()
+        {
+            InitializeComponent();
+        }
+
+        #region SocketServer_Load
+        private void SocketServer_Load(object sender, EventArgs e)
+        {
+            //取消跨线程检查
+            Control.CheckForIllegalCrossThreadCalls = false;
+        }
+        #endregion
+
+
+        /******************************
+         ** 按钮：启动服务器
+         */
+        #region btnStartBind_Click
+        private void btnStartBind_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                socketWatch = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                IPEndPoint point = new IPEndPoint(IPAddress.Parse("127.0.0.1"), Convert.ToInt32("10086"));
+                socketWatch.Bind(point);
+                Logger("启动服务器成功。");
+                socketWatch.Listen(10);
+
+                //启动线程
+                Thread th = new Thread(Accept);
+                th.IsBackground = true;
+                th.Start(socketWatch);
+
+                //禁用StartBind button
+                btnStartBind.Enabled = false;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        #endregion
+
+
+        /******************************
+         ** 按钮：发送数据
+         */
+        #region btnSendMsg_Click
+        private void btnSendMsg_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string strMsg = txtMsg.Text.Trim();
+                byte[] buffer = Encoding.UTF8.GetBytes(strMsg);
+                string selectedIP = cboUserList.SelectedItem.ToString();
+                dicSocket[ZigBee.ToString()].Send(buffer);
+
+                ShowMsgLocal(string.Format("{0}--{1}:\r\n{2}", "LocalHost", DateTime.Now.ToString(), strMsg));
+                //清除文本框中的内容
+                txtMsg.Clear();
+            }
+            catch (Exception)
+            {
+            }
+        }
+        #endregion
+
+
+        /******************************
+         ** Accept：接收连接请求
+         */
+        #region Accept
+        private void Accept(object socketObj)
+        {
+            try
+            {
+                Socket socketWatch = socketObj as Socket;
+                while (true)
+                {
+                    socketSend = socketWatch.Accept();
+                    Logger(socketSend.RemoteEndPoint.ToString() + "连接成功");
+                    dicSocket.Add(socketSend.RemoteEndPoint.ToString(), socketSend);
+                    cboUserList.Items.Add(socketSend.RemoteEndPoint.ToString());
+
+                    //设置Combobox的默认值
+                    if (cboUserList.Items.Count > 0)
+                    {
+                        cboUserList.SelectedIndex = 0;
+                    }
+
+                    //启动线程
+                    Thread th = new Thread(Receive);
+                    th.IsBackground = true;
+                    th.Start();
+                }
+            }
+            catch (Exception)
+            {
+            }
+        }
+        #endregion
+
+
+        /******************************
+         ** Receive：接收数据
+         */
+        #region Receive
+        private void Receive()
+        {
+            try
+            {
+                while (true)
+                {
+                    byte[] buffer = new byte[1024 * 1024 * 5];
+                    int r = socketSend.Receive(buffer);
+                    if (r == 0)
+                    {
+                        break;
+                    }
+                    string strMsg = Encoding.UTF8.GetString(buffer, 0, r);
+
+                    switch(strMsg.Substring(0,2)){
+
+                        /* ZigBee端消息处理 */ 
+                        case "ZB":  
+                            ZigBee = socketSend.RemoteEndPoint.ToString();  //保存ZigBee网络地址
+                            doZigBee(strMsg);                               //ZigBee端消息处理
+                            break;
+
+                        /* WeChat端消息处理 */ 
+                        case "WC":  
+                            WeChat = socketSend.RemoteEndPoint.ToString();  //保存WeChat网络地址
+                            doWeChat(strMsg);                               //WeChat端消息处理
+                            break;
+
+                        /* WinPC端消息处理 */ 
+                        case "PC":  
+                            WinPC = socketSend.RemoteEndPoint.ToString();   //保存WinPC网络地址
+                            doWinPC(strMsg);                                //WinPC端消息处理
+                            break;
+                    }         
+                }
+            }
+            catch (Exception)
+            {
+            }
+
+        }
+        #endregion
+
+
+
+        //private void MsgSend(string msg,EndPoint SS) {
+        //    try
+        //    {
+        //        //string strMsg = txtMsg.Text.Trim();
+        //        byte[] buffer = Encoding.UTF8.GetBytes(msg);
+        //        //string selectedIP = cboUserList.SelectedItem.ToString();
+        //        socketSend.SendTo(buffer,SS);
+        //        Logger(SS.ToString());
+               
+        //        //清除文本框中的内容
+        //        //txtMsg.Clear();
+        //    }
+        //    catch (Exception)
+        //    {
+        //    }
+        //}
+
+
+        #region Handle
+        /******************************
+         ** doZigBee：ZigBee端消息处理
+         */
+        private void doZigBee(string str) 
+        {
+            string sqlcmd;
+            string newstr = str.Remove(0, 2);
+            SendData(newstr, WinPC);
+            Logger("ZigBee向WinPC转发数据：" + newstr);
+            if(str.Substring(2,1)=="A"){    //数据：ZBA...........
+                for (int i = 4; i <= str.Length; i++) {
+                    if (i < 13)
+                    {
+                        sqlcmd = "UPDATE parking SET Pstatus="+ str.Substring(i-1,1)+ " WHERE Pid='A00"+ (i - 3).ToString()+ "'";
+                    }
+                    else {
+                        sqlcmd = "UPDATE parking SET Pstatus=" + str.Substring(i - 1, 1) + " WHERE Pid='A0" + (i - 3).ToString() + "'";
+                    }
+                    MySqlHelper.ExecuteNonQuery(MySqlHelper.Conn, CommandType.Text, sqlcmd , null);     //更新数据库
+                    Logger("更新数据库：" + sqlcmd);
+                }
+                    
+            }
+            else if (str.Substring(2, 1) == "B")    //数据：ZBB...........
+            {
+                for (int i = 4; i <= str.Length; i++)
+                {
+                    if (i < 13)
+                    {
+                        sqlcmd = "UPDATE parking SET Pstatus=" + str.Substring(i - 1, 1) + " WHERE Pid='B00" + (i - 3).ToString() + "'";
+                    }
+                    else
+                    {
+                        sqlcmd = "UPDATE parking SET Pstatus=" + str.Substring(i - 1, 1) + " WHERE Pid='B0" + (i - 3).ToString() + "'";
+                    }
+                    MySqlHelper.ExecuteNonQuery(MySqlHelper.Conn, CommandType.Text, sqlcmd, null);   //更新数据库
+                    Logger("更新数据库：" + sqlcmd);
+                }
+            }
+            else if (str.Substring(2, 5) == "LIGHT")    //数据：ZBLIGHT...........
+            {
+                sqlcmd = "UPDATE light SET lx='" + str.Substring(7, 5) + "',state=" + str.Substring(22, 1);
+                MySqlHelper.ExecuteNonQuery(MySqlHelper.Conn, CommandType.Text, sqlcmd, null);      //更新数据库
+                Logger("更新数据库：" + sqlcmd);
+            }
+            
+        }
+
+
+        /******************************
+         ** doWeChat：WeChat端消息处理
+         */
+        private void doWeChat(string str)
+        {
+            //DataTable table = MySqlHelper.GetDataSet(MySqlHelper.Conn, CommandType.Text, "select * from parking", null).Tables[0];
+            //DataRowCollection rows = table.Rows;
+            //for (int i = 0; i < rows.Count; i++)
+            //{
+            //    DataRow row = rows[i];
+            //    string msg = (string)row["Pid"];
+            //}
+        }
+
+
+        /******************************
+         ** doWinPC：WinPC端消息处理
+         */
+        private void doWinPC(string str)
+        {
+            string newstr = str.Remove(0, 2);
+            switch (newstr)
+            {
+                case "LIGHTON":
+                    SendData(newstr, ZigBee);
+                    Logger("WinPC向ZigBee转发数据：" + newstr);
+                    break;
+                case "LIGHTOFF":
+                    SendData(newstr, ZigBee);
+                    Logger("WinPC向ZigBee转发数据：" + newstr);
+                    break;
+                case "MANUAL":
+                    SendData(newstr, ZigBee);
+                    Logger("WinPC向ZigBee转发数据：" + newstr);
+                    break;
+                case "SELFCONTROL":
+                    SendData(newstr, ZigBee);
+                    Logger("WinPC向ZigBee转发数据：" + newstr);
+                    break;
+                default: 
+                    break;
+            }
+
+        }
+        #endregion
+
+        /******************************
+         ** SendData：发送数据给指定的端口
+         */
+        private void SendData(string msg,string endpoint) {
+            try
+            {
+                byte[] buffer = Encoding.UTF8.GetBytes(msg);
+                dicSocket[endpoint].Send(buffer);
+            }
+            catch (Exception)
+            {
+
+            }
+        }
+
+        /******************************
+         ** Logger：窗口显示发送的数据内容
+         */
+        #region Logger
+        private void Logger(string strMsg)
+        {
+            txtLog.AppendText("<--服务日志-->" + strMsg + "\r\n");
+        }
+        #endregion
+
+        #region ShowMsgLocal
+        private void ShowMsgLocal(string strMsg)
+        {
+            txtLog.AppendText("<--服务日志-->" + strMsg + "\r\n");
+        }
+        #endregion
+
+        private void SocketServer_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            try
+            {
+                socketSend.Close();
+            }
+            catch (Exception)
+            {
+            }
+        }
+
+    }
+}
